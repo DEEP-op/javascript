c82fae1b11531e0917ea48b92280801b319b1ea8
Samardeep Singh <99530213+DEEP-op@users.noreply.github.com>
2024-03-30 21:34:26 +0530

    Update 20_map_filter_reduce.js

--- 20_map_filter_reduce.js
+++ 20_map_filter_reduce.js
@@ -20,7 +20,7 @@ let z= arr2.filter((value)=>{
 console.log(z);
 
 //reduce method 
-//adds 1+2=result , result+3= result, result+4 = result.....
+//adds 1+2=result , result+3= result, result+4 = result....
 let arr3 = [1,4,6,7,4,4,7,]
 let x = arr3.reduce((h1 ,h2)=>{
     return h1 +h2
